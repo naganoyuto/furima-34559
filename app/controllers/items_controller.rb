aae7a35de26a138cd59f501e40dd803e8170bd15
singori <naganoyuto@icloud.com>
2021-03-01 11:41:57 +0900

    レビュー後の修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in?
-      @item = Item.find(params[:id])
+    if current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
     end
