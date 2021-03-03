ff422d44c5df48c1713878d2fb17428ed60709ca
singori <naganoyuto@icloud.com>
2021-03-03 12:48:20 +0900

    テーブル作成

--- db/migrate/20210303020918_create_shipping_addresses.rb
+++ db/migrate/20210303020918_create_shipping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
-
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :municipalities, null: false
+      t.string :address, null: false
+      t.string :builing_name
+      t.string :phone_number, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
