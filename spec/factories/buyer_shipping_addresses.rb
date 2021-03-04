d84a6ab6360f96ebf3dc4931b9ec6b050bca8755
singori <naganoyuto@icloud.com>
2021-03-04 12:16:23 +0900

    単体テストコード1

---
+++ spec/factories/buyer_shipping_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :buyer_shipping_address do
+    postal_code {'123-4567'}
+    prefecture_id {1}
+    municipalities {"小林市"}
+    address {"青山1-1-1"}
+    builing_name {"柳ビル103"}
+    phone_number{"09033334444"}
+  end
+end
